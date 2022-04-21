2bc6cfe35c86fb2fa05596222978de00165c94be
KikiDotPy <1chiara.disano@gmail.com>
2022-04-21 19:00:36 +0200

    making week day ex case insensitive

--- Class 12/class-weekend-boring/js/main.js
+++ Class 12/class-weekend-boring/js/main.js
@@ -2,12 +2,14 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase()
+
+
 
   //Conditionals go here
-  if (day === "Tuesday" || day === "Thursday"){
+  if (day === "tuesday" || day === "thursday"){
     document.querySelector("#placeToSee").innerHTML = "Class Time!"
-  } else if (day === "Saturday" || day === "Sunday") {
+  } else if (day === "taturday" || day === "tunday") {
     document.querySelector("#placeToSee").innerHTML = "Weekend Finally!!!"
   } else {
     document.querySelector("#placeToSee").innerHTML = "Booooring.."
