d6ac5e11da971d5b2043cdbd794fa7ad9d417330
KikiDotPy <1chiara.disano@gmail.com>
2022-04-21 18:53:26 +0200

    JS week day ex

--- Class 12/class-weekend-boring/js/main.js
+++ Class 12/class-weekend-boring/js/main.js
@@ -5,6 +5,12 @@ function check() {
   const day = document.querySelector('#day').value
 
   //Conditionals go here
-
+  if (day === "Tuesday" || day === "Thursday"){
+    document.querySelector("#placeToSee").innerHTML = "Class Time!"
+  } else if (day === "Saturday" || day === "Sunday") {
+    document.querySelector("#placeToSee").innerHTML = "Weekend Finally!!!"
+  } else {
+    document.querySelector("#placeToSee").innerHTML = "Booooring.."
+  }
 
 }
