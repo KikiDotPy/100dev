85fc19d37299081c3a748d88a306f7713800e74a
KikiDotPy <1chiara.disano@gmail.com>
2022-04-22 12:29:34 +0200

    add bad calculator

---
+++ Class 12/Copies/main.js
@@ -0,0 +1,14 @@
+// create a global variable for result
+let result;
+
+// add event listener to the element and let it call a function
+document.getElementById("").addEventListener("click", zero);
+
+
+
+
+// create a function for each element which interact with the result
+function zero() {
+    result = 0;
+    document.querySelector("#result").innerHTML = "result";
+}
