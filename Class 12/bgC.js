b50ecd4457d1a70edac006b9d8d57a1d1582793f
KikiDotPy <1chiara.disano@gmail.com>
2022-04-22 12:15:19 +0200

    Add background picker

---
+++ Class 12/bgC.js
@@ -0,0 +1,36 @@
+// BACKGOUND PICKER
+
+// put event listener to element choice and let it call a function
+document.getElementById("blue").onclick = first;
+document.getElementById("green").onclick = second;
+document.getElementById("yellow").onclick = third;
+document.getElementById("orange").onclick = fourth;
+document.getElementById("red").onclick = fifth;
+
+
+
+// create the function with the desired output
+function first() {
+    document.querySelector("body").style.backgroundColor = "rgba(171, 197, 223)";
+    document.querySelector("body").style.color = "white";
+}
+
+function second() {
+    document.querySelector("body").style.backgroundColor = "rgbs(199, 233, 204)";
+    document.querySelector("body").style.color = "white";
+}
+
+function third() {
+     document.querySelector("body").style.backgroundColor = "rgba(245, 238, 196)";
+     document.querySelector("body").style.color = "white";
+}
+
+function fourth() {
+    document.querySelector("body").style.backgroundColor = "rgba(236, 204, 170)";
+    document.querySelector("body").style.color = "white";
+}
+
+function fifth() {
+    document.querySelector("body").style.backgroundColor = "rgba(235, 171, 168)";
+    document.querySelector("body").style.backgroundColor = "white";
+}
