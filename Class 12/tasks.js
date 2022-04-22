297adec40c4f929e090059a23b6148db8603fde6
KikiDotPy <1chiara.disano@gmail.com>
2022-04-22 10:33:21 +0200

    Add function tasks

--- Class 12/tasks.js
+++ Class 12/tasks.js
@@ -13,3 +13,26 @@ let currentUser;
 const BIRTHDAY = '18.04.1982';
 
 const age = someCode(BIRTHDAY)
+
+
+/*  ###############
+    FUNCTIONS TASKS 
+    ############### */
+
+function checkAge(age) {
+    return (age > 18) || confirm('Did parents allow you?');
+}
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+
+function pow(x, n) {
+    let result = x;
+
+    for(let i = 0; i < n; i++) {
+        result *= x;
+    }
+    
+    return result;
+}
