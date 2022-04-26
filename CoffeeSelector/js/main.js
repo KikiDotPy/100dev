234a203bd21e3c84ca5832271cd5170b25dd9cf5
KikiDotPy <1chiara.disano@gmail.com>
2022-04-26 22:49:32 +0200

    link

--- CoffeeSelector/js/main.js
+++ CoffeeSelector/js/main.js
@@ -9,7 +9,7 @@ function bgCozy() {
 }
 
 function bgCalm() {
-    document.querySelector("body").style.backgroundImage = "url()"
+    document.querySelector("body").style.backgroundImage = "url(https://images.unsplash.com/photo-1588066080712-b972871ee36b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80)"
     document.querySelector("body").style.backgroundSize = "cover"
     document.querySelector("body").style.backgroundRepeat = "no-repeat"
 }
