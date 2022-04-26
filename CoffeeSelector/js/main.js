b9c9dc978b0618c7925aa758856c9ad8666ae142
KikiDotPy <1chiara.disano@gmail.com>
2022-04-26 22:46:54 +0200

    Add bg links

--- CoffeeSelector/js/main.js
+++ CoffeeSelector/js/main.js
@@ -3,7 +3,19 @@ document.getElementById("calm").addEventListener("click", bgCalm);
 document.getElementById("chill").addEventListener("click", bgChill);
 
 function bgCozy() {
-    document.querySelector("body").style.backgroundImage = "url(./img/bg3.png)"
+    document.querySelector("body").style.backgroundImage = "url(https://images.unsplash.com/photo-1559235387-25d3011f5ba8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80)"
+    document.querySelector("body").style.backgroundSize = "cover"
+    document.querySelector("body").style.backgroundRepeat = "no-repeat"
+}
+
+function bgCalm() {
+    document.querySelector("body").style.backgroundImage = "url()"
+    document.querySelector("body").style.backgroundSize = "cover"
+    document.querySelector("body").style.backgroundRepeat = "no-repeat"
+}
+
+function bgChill() {
+    document.querySelector("body").style.backgroundImage = "url(https://images.unsplash.com/photo-1525193612562-0ec53b0e5d7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80)"
     document.querySelector("body").style.backgroundSize = "cover"
     document.querySelector("body").style.backgroundRepeat = "no-repeat"
 }
