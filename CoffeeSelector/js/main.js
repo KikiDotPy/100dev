eeedf8ca95a39ba085fd97bc91b4dfba9fe95e11
KikiDotPy <1chiara.disano@gmail.com>
2022-04-26 20:27:43 +0200

    add bg image

--- CoffeeSelector/js/main.js
+++ CoffeeSelector/js/main.js
@@ -1,7 +1,9 @@
-document.getElementById("cozy").addEventListener("click", bgCozy);
+document.getElementById("cozy").onclick = bgCozy;
 document.getElementById("calm").addEventListener("click", bgCalm);
 document.getElementById("chill").addEventListener("click", bgChill);
 
 function bgCozy() {
-    document.querySelector("body").style.background("url()")
+    document.querySelector("body").style.backgroundImage = "url(./img/bg3.png)"
+    document.querySelector("body").style.backgroundSize = "cover"
+    document.querySelector("body").style.backgroundRepeat = "no-repeat"
 }
