f60ee868a025cc8df302705eadaaf062335d7504
KikiDotPy <1chiara.disano@gmail.com>
2022-04-26 19:38:48 +0200

    Add bgimages

---
+++ CoffeeSelector/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("cozy").addEventListener("click", bgCozy);
+document.getElementById("calm").addEventListener("click", bgCalm);
+document.getElementById("chill").addEventListener("click", bgChill);
+
+function bgCozy() {
+    document.querySelector("body").style.background("url()")
+}
