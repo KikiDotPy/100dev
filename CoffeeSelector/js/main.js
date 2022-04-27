c3d40460675267f980f00cde9a8b4a0f81f5a98b
KikiDotPy <1chiara.disano@gmail.com>
2022-04-27 10:08:25 +0200

    Add display value for hide box

--- CoffeeSelector/js/main.js
+++ CoffeeSelector/js/main.js
@@ -6,16 +6,19 @@ function bgCozy() {
     document.querySelector("body").style.backgroundImage = "url(https://images.unsplash.com/photo-1559235387-25d3011f5ba8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80)"
     document.querySelector("body").style.backgroundSize = "cover"
     document.querySelector("body").style.backgroundRepeat = "no-repeat"
+    document.getElementById("drink").style.display = "flex"
 }
 
 function bgCalm() {
     document.querySelector("body").style.backgroundImage = "url(https://images.unsplash.com/photo-1588066080712-b972871ee36b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80)"
     document.querySelector("body").style.backgroundSize = "cover"
     document.querySelector("body").style.backgroundRepeat = "no-repeat"
+    document.getElementById("drink").style.display = "flex"
 }
 
 function bgChill() {
     document.querySelector("body").style.backgroundImage = "url(https://images.unsplash.com/photo-1525193612562-0ec53b0e5d7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80)"
     document.querySelector("body").style.backgroundSize = "cover"
     document.querySelector("body").style.backgroundRepeat = "no-repeat"
+    document.getElementById("drink").style.display = "flex"
 }
