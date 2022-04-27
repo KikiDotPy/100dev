ca98eaa0c39bb72966cca6c8311ccd21d9cbc94e
KikiDotPy <1chiara.disano@gmail.com>
2022-04-27 10:26:16 +0200

    pseudo code

--- Week 7/Class 13/temperature-converter/js/main.js
+++ Week 7/Class 13/temperature-converter/js/main.js
@@ -1,1 +1,10 @@
 //Write your pseduo code first! 
+
+// put event listener on a button or element
+// call a function to convert the value
+
+// structure function
+// put id on the input and take and store the input value in a variable
+// do the math in a new variable
+// put the new value into HTML element
+
