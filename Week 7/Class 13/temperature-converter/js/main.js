1190d4f0c7dbd2df1b58675a5327b85f956eb760
KikiDotPy <1chiara.disano@gmail.com>
2022-04-27 10:40:05 +0200

    Add js to CelToFar converter

--- Week 7/Class 13/temperature-converter/js/main.js
+++ Week 7/Class 13/temperature-converter/js/main.js
@@ -1,7 +1,13 @@
 //Write your pseduo code first! 
 
 // put event listener on a button or element
+document.getElementById("check").onclick = convert;
 // call a function to convert the value
+ function convert() {
+     let sel = document.querySelector("#celsius").value;
+     sel = sel * 9/5 + 32;
+     document.getElementById("placeForValu").innerText = celsius;
+ }
 
 // structure function
 // put id on the input and take and store the input value in a variable
