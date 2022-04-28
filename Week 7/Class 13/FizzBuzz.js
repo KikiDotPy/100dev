bda1b84c1e0b7ea1b798b210f355289ad079fa7c
KikiDotPy <1chiara.disano@gmail.com>
2022-04-28 11:15:17 +0200

    FizzBuzz func

--- Week 7/Class 13/FizzBuzz.js
+++ Week 7/Class 13/FizzBuzz.js
@@ -5,7 +5,9 @@ It shows "Fizz" instead if the number is divisible by 3.
 It shows "Buzz" instead if the number is divisible by 5 and not by 3. */
 
 for (let i = 1; i <= 100; i++){
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
         console.log("Fizz");
     } else if (i % 5 === 0) {
         console.log("Buzz");
