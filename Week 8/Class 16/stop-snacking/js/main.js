454c1b1a612c6068707d3b9ab94a5750b74a861e
KikiDotPy <1chiara.disano@gmail.com>
2022-05-10 19:16:22 +0200

    Done stopSnack exercise

--- Week 8/Class 16/stop-snacking/js/main.js
+++ Week 8/Class 16/stop-snacking/js/main.js
@@ -1,1 +1,11 @@
-//Create a function that grabs the number of snacks from the input and tells you to stop that many times
+//Create a function that grabs the number of snacks from the input and tells you to stop that 
+//many times
+
+document.querySelector('#help').addEventListener('click', noSnack)
+
+function noSnack() {
+    const input = Number(document.querySelector('input').value);
+    for(let i = 0; i < input; i++) {
+        document.querySelector('#stops').innerText += " Stop Snacking!!!";
+    }
+}
