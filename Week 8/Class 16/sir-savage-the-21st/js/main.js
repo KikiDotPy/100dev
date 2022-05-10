57ecd0d6a794236ecd1e83b71073a6997b9d6609
KikiDotPy <1chiara.disano@gmail.com>
2022-05-10 19:08:34 +0200

    Done loop exercise

--- Week 8/Class 16/sir-savage-the-21st/js/main.js
+++ Week 8/Class 16/sir-savage-the-21st/js/main.js
@@ -1,2 +1,13 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+function tNum() {
+    const n = 21;
+    for (i = 0; i < 21; i++) {
+        console.log(n)
+    }
+}
+
+tNum()
 //Bonus can you make it print '21' 21 times to the dom?
+function tNum() {
+    document.querySelector('#savageSays').innerText += ' 21'
+}
