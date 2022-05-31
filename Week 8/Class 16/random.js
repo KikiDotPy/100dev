b70ef4a8a9e442b20bef559ed96753edb7f35575
KikiDotPy <1chiara.disano@gmail.com>
2022-05-31 13:03:00 +0200

    conditional exercises

--- Week 8/Class 16/random.js
+++ Week 8/Class 16/random.js
@@ -1,4 +1,4 @@
-let a = 2;
+/* let a = 2;
 a -= 1;
 a++;
 let b = 8;
@@ -9,4 +9,51 @@ const d = a * b + b;
 const e = a * (b + b);
 const f = a * b / a;
 const g = b / a * a;
-console.log(a, b, c, d, e, f, g);
+console.log(a, b, c, d, e, f, g); */
+
+/* const day = "Monday".toLowerCase();
+const weekDays = ["Monday", "Tuesday", "Wednsday", "Thuersday", "Friday", "Saturday", "Sunday"];
+
+switch (day) {
+    case "monday":
+        console.log(weekDays[1]);
+        break;
+    case "tuesday":
+        console.log(weekDays[2]);
+        break;
+    case "wednsday":
+        console.log(weekDays[3]);
+        break;
+    case "thuersday":
+        console.log(weekDays[4]);
+        break;
+    case "friday":
+        console.log(weekDays[5]);
+        break;
+    case "saturday":
+        console.log(weekDays[6]);
+        break;
+    case "sunday":
+        console.log(weekDays[0]);
+        break;
+    default:
+        console.log("Wrong day name");
+} */
+
+let nb1, nb2, nb3;
+nb1 = 2;
+nb2 = 4;
+nb3 = 0;
+
+if (nb1 > nb2) {
+    nb1 = nb3 * 2;
+} else {
+    nb1++;
+    if (nb2 > nb3) {
+        nb1 += nb3 * 3;
+    } else {
+        nb1 = 0;
+        nb3 = nb3 *2 + nb2;
+    }
+}
+console.log(nb1, nb2,nb3);
