31380bbc293c0729a07faf8c34c2e7369f827a21
KikiDotPy <1chiara.disano@gmail.com>
2022-06-01 09:41:46 +0200

    Refactor with loop

--- Week 8/Class 16/random.js
+++ Week 8/Class 16/random.js
@@ -64,10 +64,6 @@ function square1(x) {
 
 const square2 = x => x * x;
 
-console.log(square1(0));
-console.log(square1(2));
-console.log(square1(5));
-
-console.log(square2(0));
-console.log(square2(2));
-console.log(square2(5));
+for(let i = 0; i <= 10; i++) {
+    console.log(square2(i));
+}
