da0ffcc2b6af94f5acb2020c7b1a655a9ef331d7
KikiDotPy <1chiara.disano@gmail.com>
2022-06-06 16:51:22 +0200

    fixed forloop

--- Week 8/Class 19/bring-it-on-all-or-nothing/js/main.js
+++ Week 8/Class 19/bring-it-on-all-or-nothing/js/main.js
@@ -37,7 +37,7 @@ function trueOrNot (bol, str) {
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
 function counting (n) {
-    for (let i = 0; i < n; i++) {
+    for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
         } else if (i % 3 === 0) {
