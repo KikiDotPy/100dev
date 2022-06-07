acfe059c4c582720616df341b3d7c5bce23da44d
KikiDotPy <1chiara.disano@gmail.com>
2022-06-07 10:47:30 +0200

    fix

--- Week 8/Class 19/arrays-fight-to-the-finish/js/main.js
+++ Week 8/Class 19/arrays-fight-to-the-finish/js/main.js
@@ -1,12 +1,11 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 const movies = ["Spiderman into Spiderverse", "Isle of Dogs", "Pulp Fiction"];
-const h2 = document.querySelector("h2").innerHTML;
-movies.forEach((e) => h2 = e);
+movies.forEach((e) => document.querySelector("h2").innerText += e);
 
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
-const numbers = [1, 3, 4, 5, 10];
-numbers.forEach((n) => n += 3);
+const numbers = [10, 40, 30];
+numbers.forEach((n, i) => numbers[i] = n + 3);
 
 //Find the average of all the numbers from question three
 const nLen = numbers.length;
