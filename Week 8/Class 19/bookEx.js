6def30c2099a769fc0c7f5b4cfb6e765e8814bc7
KikiDotPy <1chiara.disano@gmail.com>
2022-06-08 12:30:08 +0200

    book exercises obj

---
+++ Week 8/Class 19/bookEx.js
@@ -0,0 +1,18 @@
+// Improve char Obj /w exp prop
+
+const Aurora = {
+    name = "Aurora",
+    health = 150,
+    strenght = 25,
+    exp = 0;
+}
+
+// Aurora is harmed by an arrow
+Aurora.health -= 20;
+
+// Aurora equip strengh accessory
+Aurora.strenght += 10;
+
+// Aurora kills a monster
+
+Aurora.exp += 100;
