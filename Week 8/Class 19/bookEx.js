933b4e2b6c6f2728bd0dc4698624999489c690f5
KikiDotPy <1chiara.disano@gmail.com>
2022-06-08 12:42:44 +0200

    book exercises obj

--- Week 8/Class 19/bookEx.js
+++ Week 8/Class 19/bookEx.js
@@ -1,6 +1,6 @@
 // Improve char Obj /w exp prop
 
-const Aurora = {
+/* const Aurora = {
     name = "Aurora",
     health = 150,
     strenght = 25,
@@ -15,4 +15,20 @@ Aurora.strenght += 10;
 
 // Aurora kills a monster
 
-Aurora.exp += 100;
+Aurora.exp += 100; */
+
+const Alex = {
+    name : "Alex",
+    balance : 0,
+    
+    credit(n) {
+        balance += n;
+    },
+
+    describe() {
+        return `owner: ${this.name}, balance ${this.balance}`;
+    }
+};
+
+Alex.credit(250);
+console.log(Alex.describe());
