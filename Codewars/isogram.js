c52d63fe95e2c5951697da0414d581d267babba9
KikiDotPy <1chiara.disano@gmail.com>
2022-06-10 16:54:44 +0200

    Daily codewars solution

---
+++ Codewars/isogram.js
@@ -0,0 +1,17 @@
+function isIsogram(str){
+    //...
+    str = str.toLowerCase();
+    if (str.length === 0) {
+      return true;
+    }
+    for (let i = 0; i < str.length; i++) {
+      for (let j = i+1; j < str.length; j++) {
+        if (str[i] === str[j]){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  
