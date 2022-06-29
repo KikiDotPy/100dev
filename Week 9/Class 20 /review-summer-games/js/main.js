db8dc8ef046ca33a146452d961d3bef7e01ffcf6
KikiDotPy <1chiara.disano@gmail.com>
2022-06-29 10:46:24 +0200

    100dev ex

--- Week 9/Class 20 /review-summer-games/js/main.js
+++ Week 9/Class 20 /review-summer-games/js/main.js
@@ -1,1 +1,5 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multyArr(arr) {
+    const len = arr.length;
+    arr.forEach( (e, i) => alert(arr[i] * arr[i + 1]);
+}
