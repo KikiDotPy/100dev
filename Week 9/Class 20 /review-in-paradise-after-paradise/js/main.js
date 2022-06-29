78405602b6c787a831159f056884561775a10da8
KikiDotPy <1chiara.disano@gmail.com>
2022-06-29 10:32:16 +0200

    fix

--- Week 9/Class 20 /review-in-paradise-after-paradise/js/main.js
+++ Week 9/Class 20 /review-in-paradise-after-paradise/js/main.js
@@ -8,7 +8,7 @@ function check(arr) {
         alert("Hi")
     } else if (arr[0] > arr[arr.length - 1]) {
         alert("Bye")
-    } else if (arr[0] === arr[arr.length - 1]) {
+    } else {
         alert("We close in an hour")
     }
     
