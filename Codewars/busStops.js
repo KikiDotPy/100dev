f44daae9fb8388e0ce9240ec217487d0ef303c59
KikiDotPy <1chiara.disano@gmail.com>
2022-06-29 09:54:50 +0200

    refactoring

--- Codewars/busStops.js
+++ Codewars/busStops.js
@@ -2,3 +2,5 @@ var number = function(busStops){
     // Good Luck!
     return busStops.reduce((sum, arr) => sum += arr[0] - arr[1], 0);
   }
+
+  const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
