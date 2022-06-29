a066ac4c9a899505a81ab8e40ada2fe78753ee75
KikiDotPy <1chiara.disano@gmail.com>
2022-06-29 11:24:24 +0200

    100dev ex

--- Week 9/Class 20 /review-winter-games/js/main.js
+++ Week 9/Class 20 /review-winter-games/js/main.js
@@ -1,1 +1,10 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers. Return a new array containing every even number from the 
+//original array (do not use map or filter)
+
+function evenArr (arr) {
+    let newArr = [];
+    for (e of arr) {
+        (e % 2 === 0) ? newArr.push(e) : 0;
+    }
+    return newArr;
+}
