a9d16214713e90d58f26f12340f82408cc4b38a2
KikiDotPy <1chiara.disano@gmail.com>
2022-07-01 22:37:12 +0200

    obj exercise

--- Week 9/Class 21/objects-zeo/js/main.js
+++ Week 9/Class 21/objects-zeo/js/main.js
@@ -1,1 +1,11 @@
 //Create a stopwatch object that has four properties and three methods
+const stopwatch = {};
+
+stopwatch.color = "Black";
+stopwatch.shape = "Round";
+stopwatch.brand = "Abrand";
+stopwatch.minutes = 45;
+
+stopwatch.start = () => console.log("Started");
+stopwatch.end = () => console.log("Finished");
+stopwatch.bip = () => console.log("BipBip!");
