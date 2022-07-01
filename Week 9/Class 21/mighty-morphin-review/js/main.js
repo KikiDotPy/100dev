6912af4a7eb586085b60d44b36938a96a58668c8
KikiDotPy <1chiara.disano@gmail.com>
2022-07-01 21:42:22 +0200

    ex fix

--- Week 9/Class 21/mighty-morphin-review/js/main.js
+++ Week 9/Class 21/mighty-morphin-review/js/main.js
@@ -10,21 +10,32 @@ alert(str.slice(-3));
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
 function subAlert(n1,n2,n3,n4,n5) {
-    let sum = 100;
-    return sum - n1 - n2 - n3 - n4 - n5;
+    let sum = 100 - n1 - n2 - n3 - n4 - n5;
+    return Math.abs(sum);
 }
 
 subAlert(2,3,4,5,6);
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
 function consoleLower(n1,n2,n3) {
-    
+
 }
 
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
+function headOrTail() {
+    const num = Math.floor( Math.random() * 2 );
+    return num === 1 ? "Tail" : "Head";
+}
 
 
 //*Loops*
-//Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
+//Create a function that takes in a number. Console log the result of heads or tails using the previous function x 
+//times where x is the number passed into the function. Call the function.
+function consoleTH(n) {
+    for (let i = 0; i < n; i++) {
+        let num = headOrTail();
+        console.log(num);
+    }
+}
