e857de027f32e40e95d474fc2fb5f9b59ea788f3
KikiDotPy <1chiara.disano@gmail.com>
2022-07-01 21:35:03 +0200

    class exercise

--- Week 9/Class 21/mighty-morphin-review/js/main.js
+++ Week 9/Class 21/mighty-morphin-review/js/main.js
@@ -1,13 +1,25 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
+let favHoliday = "CHRISTMAS";
+console.log(favHoliday);
 
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+const str = "Woahh!";
+alert(str.slice(-3));
 
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
+function subAlert(n1,n2,n3,n4,n5) {
+    let sum = 100;
+    return sum - n1 - n2 - n3 - n4 - n5;
+}
 
+subAlert(2,3,4,5,6);
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
+function consoleLower(n1,n2,n3) {
+    
+}
 
 
 // *Conditionals*
