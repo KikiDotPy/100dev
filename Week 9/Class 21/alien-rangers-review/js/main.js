bc9d10a6ad468f40f4a04eb23ea035576d2bdbde
KikiDotPy <1chiara.disano@gmail.com>
2022-07-01 22:21:53 +0200

    fix typo

--- Week 9/Class 21/alien-rangers-review/js/main.js
+++ Week 9/Class 21/alien-rangers-review/js/main.js
@@ -14,5 +14,5 @@ console.log(numEven(nums));
 //Alert the sum of the second lowest and the second highest number
 function sumLowHigh (arr) {
     const sorted = arr.sort((a,b) => a - b);
-    alert(sort[1] + sort[-2]);
+    alert(sorted[1] + sorted[sorted.length - 2]);
 }
