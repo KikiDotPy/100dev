c229d96990d80dbc21605b3aff8366d805bb653c
KikiDotPy <1chiara.disano@gmail.com>
2022-07-02 11:27:39 +0200

    constructor exercise: creating a SF character

--- Week 9/Class 21/objects-street-fighter/js/main.js
+++ Week 9/Class 21/objects-street-fighter/js/main.js
@@ -1,1 +1,21 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+class Character {
+    constructor(name, gender, sMove, bool) {
+        const name = name;
+        const gender = gender;
+        const specialMove = sMove;
+        const evil = bool;
+    }
+
+    specialMove(){
+        console.log(this.specialMove);
+    }
+
+    hello(){
+        console.log(`Hi there! I am ${this.name}`);
+    }
+
+    win(){
+        console.log("I win!");
+    }
+}
