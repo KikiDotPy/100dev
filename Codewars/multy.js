eaa6cedcafa450e01fc4bc5d155800262c040b82
KikiDotPy <1chiara.disano@gmail.com>
2022-07-02 11:33:52 +0200

    adding solution

--- Codewars/multy.js
+++ Codewars/multy.js
@@ -1,8 +1,11 @@
 function countBy(x, n) {
-    let z = [];
-    for (let i = 1; i <= n; i++){
-      z.push(x * i);
-    }
-  
-    return z;
+  let z = [];
+  for (let i = 1; i <= n; i++){
+    z.push(x * i);
   }
+
+  return z;
+}
+
+const countBy = (x, n) =>
+  [...Array(n)].map((_, idx) => ++idx * x);
