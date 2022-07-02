ece559931aff5295f0ff60d04664a27e55551f18
KikiDotPy <1chiara.disano@gmail.com>
2022-07-02 14:44:16 +0200

    Completed 4° ex

--- Week 9/Class 21/Obj.js
+++ Week 9/Class 21/Obj.js
@@ -22,7 +22,7 @@ function isEmpty(obj) {
     return true;
 }
 
-//Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above. If salaries is empty, then the result must be 0.
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above. If salaries is empty, then the result must be 0.
 
 let salaries = {
     John: 100,
@@ -35,3 +35,19 @@ for (salarie in salaries) {
     sum += salaries[salarie];
 }
 console.log(sum);
+
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+function multiplyNumeric(obj) {
+    for (item in obj){
+        if (typeof obj[item] == "number") {
+            obj[item] *= 2;
+        }
+    }
+    return obj;
+}
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  console.log(multiplyNumeric(menu))
