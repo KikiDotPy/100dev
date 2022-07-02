7612f74badb01f0b4047a5a1185f944917f46ecb
KikiDotPy <1chiara.disano@gmail.com>
2022-07-02 14:26:47 +0200

    Completed 3° ex

--- Week 9/Class 21/Obj.js
+++ Week 9/Class 21/Obj.js
@@ -13,3 +13,25 @@ console.log(user);
 delete user.name;
 
 console.log(user);
+
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+function isEmpty(obj) {
+    for (prop in obj){
+        return false;
+    } 
+    return true;
+}
+
+//Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above. If salaries is empty, then the result must be 0.
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+
+let sum = 0;
+for (salarie in salaries) {
+    sum += salaries[salarie];
+}
+console.log(sum);
