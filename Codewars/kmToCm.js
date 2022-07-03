698e54f5d7c88b773c7680e3ce01e410f9da0f50
KikiDotPy <1chiara.disano@gmail.com>
2022-07-03 14:52:08 +0200

    fix

--- Codewars/kmToCm.js
+++ Codewars/kmToCm.js
@@ -1,1 +1,9 @@
 const cockroachSpeed = s => Math.floor(s / 0.036);
+
+
+function cockroachSpeed(s) {
+    const secsInHour = 3600;
+    const centimetersInKilometers = 100000;
+    
+    return Math.floor((s*centimetersInKilometers)/secsInHour);
+  }
