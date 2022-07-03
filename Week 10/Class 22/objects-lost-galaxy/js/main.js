6ea2b72379c7feb078ce9ff434d1daebd57c777c
KikiDotPy <1chiara.disano@gmail.com>
2022-07-03 23:30:21 +0200

    ex

--- Week 10/Class 22/objects-lost-galaxy/js/main.js
+++ Week 10/Class 22/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a mouse object that has four properties and three methods
+const mouse = {}
+
+mouse.color = "grey";
+mouse.shape = "oval";
+mouse.size = "small";
+mouse.roll = true;
+
+mouse.leftClick = function(){
+    console.log("Left click!")
+}
+mouse.rightClick = function() {
+    console.log("rIGHT CLIIIIICK!")
+}
+mouse.scroll = function() {
+    console.log("roll")
+}
+
+
