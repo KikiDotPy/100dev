c61eef9ca4835ab3f89f43862cd8f0de9b656799
KikiDotPy <1chiara.disano@gmail.com>
2022-07-03 23:51:36 +0200

    Objects practice

--- Week 10/Class 22/objects-street-fighter/js/main.js
+++ Week 10/Class 22/objects-street-fighter/js/main.js
@@ -1,1 +1,10 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function StreetFighterChars(name, gender, specialMove,) {
+    this.name = name;
+    this.gender = gender;
+    this.specialMove = specialMove;
+
+    this.specialMove = function() {
+        console.log(`YOOOOOOOO ${this.specialMove}`);
+    }
+}
