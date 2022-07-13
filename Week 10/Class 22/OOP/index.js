98bb7255dcabc69fc946bbdb77f59eba0982f50c
KikiDotPy <1chiara.disano@gmail.com>
2022-07-13 14:22:05 +0200

    Create SmartWatch Object

--- Week 10/Class 22/OOP/index.js
+++ Week 10/Class 22/OOP/index.js
@@ -1,1 +1,35 @@
-console.log("hello")
+function StopWatch() {
+    let startTime, endTime, running, duration = 0
+
+    this.start = function() {
+        if (running) {
+            return "Already started!"
+        }
+        running = true
+
+        startTime = Date.now()
+    }
+
+    this.end = function() {
+        if (!running) {
+            return "Watch not started"
+        }
+
+        running = false
+
+        endTime = Date.now()
+        duration += endTime - startTime
+    }
+
+    this.callDuration = function() {
+        console.log(duration)
+    }
+
+    this.reset = function() {
+        running = false
+        duration = 0
+        starTime = null
+        endTime = null
+    }
+}
+
