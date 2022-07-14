d8951b5e003b366f6aa29a9f8cba187b128b7b34
KikiDotPy <1chiara.disano@gmail.com>
2022-07-14 19:48:31 +0200

    Constructor practice

--- Week 10/Class 23/objects-tony-hawk-pro-skater/js/main.js
+++ Week 10/Class 23/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,19 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function ProSkater(name,specialMove,jump,fightStyle) {
+    this.name = name
+    this.specialMove = specialMove
+    this.jump = jump
+    this.fightStyle = fightStyle
+
+    this.final = function(){
+        console.log(`Take this ${this.specialMove}!!!`)
+    }
+    this.win = function(){
+        console.log("I WON")
+    }
+    this.lose = function() {
+        console.log("...")
+    }
+
+}
