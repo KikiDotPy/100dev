5961a07e46a81ec9e47b3698ab3af01899bb3a20
KikiDotPy <1chiara.disano@gmail.com>
2022-07-14 12:20:22 +0200

    solved ex

--- Week 10/Class 23/space-review/js/main.js
+++ Week 10/Class 23/space-review/js/main.js
@@ -1,12 +1,28 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+const nums = [1,2,3,4,5];
+alert(nums.reduce((sum,num) => sum = num, 0))
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+function squared(nums){
+    return nums.map(n => Math.sqrt(n))
+}
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+function reverseStr(str){
+    return str.split("").reverse().join("");
+}
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+function palindrome(str) {
+    let check = reverseStr(str);
+    if (str === check){
+        alert("It's palindrome!")
+    } else {
+        alert("NOT a palindrome")
+    }
+}
