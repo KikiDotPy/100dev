cbc9c0d2aef5821b4ef8ce4d4c704d7609c26e5e
KikiDotPy <1chiara.disano@gmail.com>
2022-07-14 12:40:47 +0200

    solved object ex

--- Week 10/Class 23/objects-lost-galaxy/js/main.js
+++ Week 10/Class 23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,11 @@
 //Create a dog object that has four properties and three methods
+let dog = {}
+
+dog.color = "black"
+dog.size = "little"
+dog.tail = true
+dog.teeth = "Sharp"
+
+dog.bark = function() => {console.log("Bau!")}
+dog.happy = function() => {console.log("dog happy")}
+dog.hungry = function() => {console.log("dog hungry")}
