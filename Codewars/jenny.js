8c7d9b128983abd472ce63f362fe4a69fea21f84
KikiDotPy <1chiara.disano@gmail.com>
2022-07-20 10:22:17 +0200

    Solved daily codewars exercise

---
+++ Codewars/jenny.js
@@ -0,0 +1,10 @@
+function greet(name){
+    if(name === "Johnny")
+      return "Hello, my love!";
+    else
+      return "Hello, " + name + "!";
+  }
+
+  function greet(name){
+    return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+  }
