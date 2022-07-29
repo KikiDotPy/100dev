6622e496983e0f1cfd2cccb11adafbada731c82a
KikiDotPy <1chiara.disano@gmail.com>
2022-07-29 17:24:14 +0200

    Solved daily codewars exercise

--- Codewars/kmToCm.js
+++ Codewars/kmToCm.js
@@ -7,3 +7,10 @@ function cockroachSpeed(s) {
     
     return Math.floor((s*centimetersInKilometers)/secsInHour);
   }
+
+  function cockroachSpeed(s) {
+    //Good Luck!
+    const secInHour = 3600
+    const cmInKm = 100000
+    return Math.floor((s * cmInKm) / secInHour)
+  }
