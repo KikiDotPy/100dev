645e756f96bf7126dee00f856bf72dfe232a35bb
KikiDotPy <1chiara.disano@gmail.com>
2022-08-14 17:33:09 +0200

    Creating pizza constructor

--- Week 11/Class 26/objects-constructor-fun/js/main.js
+++ Week 11/Class 26/objects-constructor-fun/js/main.js
@@ -1,1 +1,19 @@
 //Create a constructor with 4 properties and 3 methods
+function Pizza(sauce, toppings, cheese, size){
+    this.sauce = sauce;
+    this.toppings = toppings
+    this.chees = cheese
+    this.size = size
+
+    this.cooked = function(){
+        console.log("Pizza is ready!")
+    }
+
+    this.time = function(){
+        console.log("Pizza need to cook 5 mins")
+    }
+
+    this.burnMouth = function(){
+        console.log("sjdnsjfblk..")
+    }
+}
