45901e9a08eb3ee1e81fae6101168a4106dc659d
KikiDotPy <1chiara.disano@gmail.com>
2022-08-14 17:17:57 +0200

    Created pizza Obj

--- Week 11/Class 26/objects-lost-galaxy/js/main.js
+++ Week 11/Class 26/objects-lost-galaxy/js/main.js
@@ -1,1 +1,15 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+const pizza = {};
+
+pizza.salsa = true;
+pizza.cheese = true;
+pizza.toppings = [mushrooms, peperoni, ananas];
+pizza.price = 5.30;
+
+pizza.cooked = function() {
+    console.log("Pizza is ready!")
+}
+pizza.inforna = function() {
+    console.log("Time to infornare")
+}
