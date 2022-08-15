de6cf1736c13e339b1d7a2aeee8ab3dfe27e1b1e
KikiDotPy <1chiara.disano@gmail.com>
2022-08-15 23:21:25 +0200

    added api

--- Week 11/Class 26/objects-shots/js/main.js
+++ Week 11/Class 26/objects-shots/js/main.js
@@ -1,6 +1,7 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getDrink)
 
+
 function getDrink() {
 
     let drink = document.querySelector('input').value.trim()
@@ -9,22 +10,33 @@ function getDrink() {
     .then(res => res.json())
     .then(data => {
         console.log(data.drinks)
-        
+        let max = data.drinks.length
+        let i = 0;
 
+        document.getElementById('back').addEventListener('click', goBack)
+        document.getElementById('forward').addEventListener('click', goForward)
 
+        document.querySelector('h2').innerText = data.drinks[i].strDrink
+        document.querySelector('img').src = data.drinks[i].strDrinkThumb
+        document.querySelector('h3').innerText = data.drinks[i].strInstructions
 
-
-        for (let i = 0; i < data['drinks'].length; i++){
-            setTimeout(function timer() {
-                document.querySelector('h2').innerText = data.drinks[i].strDrink
-                document.querySelector('img').src = data.drinks[i].strDrinkThumb
-                document.querySelector('h3').innerText = data.drinks[i].strInstructions
-              }, i * 3000)
+        function goBack() {
+            if (i != 0) {
+                i--
+            }
         }
+        function goForward() {
+            if (i < max) {
+                i++
+            }
+        }
+   
     })
     .catch(err => {
         console.log('error ' + err)
     })
 
 }
+
+
 
