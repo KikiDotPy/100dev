23208c68197647926e5b32d5c70dba4fb5e3cf7d
KikiDotPy <1chiara.disano@gmail.com>
2022-08-15 19:58:47 +0200

    sliding drinks

--- Week 11/Class 26/objects-shots/js/main.js
+++ Week 11/Class 26/objects-shots/js/main.js
@@ -3,15 +3,24 @@ document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink() {
 
-    let drink = document.querySelector('input').value
+    let drink = document.querySelector('input').value.trim()
 
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.drinks[0])
-        document.querySelector('h2').innerText = data.drinks[0].strDrink
-        document.querySelector('img').src = data.drinks[0].strDrinkThumb
-        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+        console.log(data.drinks)
+        
+
+
+
+
+        for (let i = 0; i < data['drinks'].length; i++){
+            setTimeout(function timer() {
+                document.querySelector('h2').innerText = data.drinks[i].strDrink
+                document.querySelector('img').src = data.drinks[i].strDrinkThumb
+                document.querySelector('h3').innerText = data.drinks[i].strInstructions
+              }, i * 3000)
+        }
     })
     .catch(err => {
         console.log('error ' + err)
