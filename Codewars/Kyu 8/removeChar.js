568dabd82d9cb0a3ca042594fe3f756f3eeb6ee5
KikiDotPy <1chiara.disano@gmail.com>
2022-08-16 10:30:16 +0200

    Organizing codewars folder

---
+++ Codewars/Kyu 8/removeChar.js
@@ -0,0 +1,10 @@
+function removeChar(str){
+    //You got this!
+     const strLen = str.length - 1
+     return str.slice(1, strLen)
+   
+   };
+
+   function removeChar(str) {
+    return str.slice(1, -1);
+  }
