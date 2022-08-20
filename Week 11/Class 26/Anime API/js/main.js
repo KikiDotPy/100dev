9d017ed8219ad5059094ebc011eb49f43b970989
KikiDotPy <1chiara.disano@gmail.com>
2022-08-20 11:12:26 +0200

    Added styles

--- Week 11/Class 26/Anime API/js/main.js
+++ Week 11/Class 26/Anime API/js/main.js
@@ -2,13 +2,15 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = 'https://animechan.vercel.app/api/random'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.getElementById('animeName').innerHTML = data.anime
+        document.getElementById('charName').innerHTML = data.character
+        document.getElementById('quote').innerHTML = `"${data.quote}"`
       })
       .catch(err => {
           console.log(`error ${err}`)
