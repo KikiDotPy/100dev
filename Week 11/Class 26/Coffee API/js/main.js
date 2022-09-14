7c0547c2c3c3946ae6af39ee8ded76f29ef758a4
KikiDotPy <1chiara.disano@gmail.com>
2022-09-14 17:29:16 +0200

    created AC api

--- Week 11/Class 26/Coffee API/js/main.js
+++ Week 11/Class 26/Coffee API/js/main.js
@@ -2,15 +2,23 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const villagerID = getRandomInt(1, 392)
+  const url = `https://acnhapi.com/v1/villagers/${villagerID}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.getElementById('villagerImage').src = data.image_uri
+        document.getElementById('hello').innerHTML = `${data['catch-phrase']}! I am ${data.name['name-USen']}, nice to meet you!`
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
