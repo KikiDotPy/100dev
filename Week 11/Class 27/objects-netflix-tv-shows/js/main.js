c6e55bfb63c5622aa24f0f0519912d643508d731
Chiara Di Sano <chiara.disano@thoughtworks.com>
2022-09-15 12:54:46 +0200

    Kiki - Netflix TvShow Constructor - Created a class to make TvShows

--- Week 11/Class 27/objects-netflix-tv-shows/js/main.js
+++ Week 11/Class 27/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,24 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class MakeNetflixTvShows {
+    constructor(tvShowTime, tvShowEpisodes, tvShowTitle, tvShowGenre) {
+        this.time = tvShowTime
+        this.numberOfEpisodes = tvShowEpisodes
+        this.title = tvShowTitle
+        this.genre = tvShowGenre
+    }
 
+    getTitle() {
+        console.log(`Have a lovely time watching ${this.title}`)
+    }
+
+    play() {
+        console.log('Show is about to start!')
+    }
+
+    stop() {
+        console.log('Show stopped')
+    }
+}
+
+const bridgerton = new MakeNetflixTvShows(60, 12, 'Bridgerton', 'Period Romantic Drama')
