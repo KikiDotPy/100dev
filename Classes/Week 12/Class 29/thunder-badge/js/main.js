89f5fc143d32144ff33e1523fd210569712b1963
Chiara Di Sano <chiara.disano@thoughtworks.com>
2022-12-09 17:25:55 +0100

    Class 29 | Kiki | Create a Pokemon Class

--- Classes/Week 12/Class 29/thunder-badge/js/main.js
+++ Classes/Week 12/Class 29/thunder-badge/js/main.js
@@ -1,2 +1,32 @@
 //Objects
 //Please create a pokemon class with as much detail as possible in 10 minutes
+
+class Pokemon {
+    constructor(name, hp, sp, type, ab, sex, weak, str){
+        this.name = name
+        this.type = type
+        this.health = hp
+        this.skillPoint = sp
+        this.strength = str
+        this.ability = ab
+        this.sex = sex
+        this.weakness = weak
+    }
+
+    attack(target){
+        if (this.health > 0) {
+            target.health -= this.strength
+        } else {
+            console.log(`${this.name} can't attack, it's too tired`)
+        }
+    }
+
+    summon(){
+        console.log(`${this.name} entra in campo con ${this.health}`)
+    }
+
+    specialAbility(target){
+        console.log(`${this.name} uses ${this.ability} on ${target}`)
+    }
+
+}
