4166eeb8b074a8c5e8352365b90d8ab1733aaae0
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-01-04 13:17:39 +0100

    Class 30 | Kiki | Finish espresso OOP ezercise

--- Classes/Week 13/Class 30/objects-espresso-machine/js/main.js
+++ Classes/Week 13/Class 30/objects-espresso-machine/js/main.js
@@ -1,1 +1,29 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine {
+    constructor(color, brand, size, capsule, grind){
+        this.color = color;
+        this.brand = brand;
+        this.size = size;
+        this.capsule = capsule;
+        this.grind = grind;
+    }
+
+    start() {
+        console.log('The machine is preparing the coffee');
+    }
+
+    finish() {
+        console.log('The coffee is ready to drink!');
+    }
+
+    grinding (){
+        if (this.grind){
+            console.log('Grinding coffee beans...');
+        } else {
+            console.log("This machine can't grind coffee beans");
+        }
+    }
+
+
+    
+}
