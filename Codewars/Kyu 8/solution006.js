3502bf3692670dafadd6554688ebb1a98cd86fe2
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-04-05 12:11:48 +0200

    Add 2 more easy and clear solution to solution6

--- Codewars/Kyu 8/solution006.js
+++ Codewars/Kyu 8/solution006.js
@@ -6,3 +6,16 @@ function updateLight(current) {
     const length = lights.length - 1
     return currentLight == 2 ? lights[length % 2] : lights[++currentLight]
   }
+
+
+const updateLight = current => ({
+  green: 'yellow',
+  yellow: 'red',
+  red: 'green',
+})[current]
+
+function updateLight(current) {
+  
+  return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+
+}
