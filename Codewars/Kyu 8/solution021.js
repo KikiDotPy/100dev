6938c6436fe066d25a2a0ae7f0fc5fc43fe30aa2
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-05-09 15:50:38 +0200

    Daily codewars solution exercise

---
+++ Codewars/Kyu 8/solution021.js
@@ -0,0 +1,17 @@
+function htmlspecialchars(formData) {
+    return formData.replace(/&/g, "&amp;")
+                   .replace(/"/g, "&quot;")
+                   .replace(/</g, "&lt;")
+                   .replace(/>/g, "&gt;");
+  }
+
+
+  function htmlspecialchars(formData) {
+    const dict = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '&': '&amp;',
+    };
+    return formData.replace(/[<>"&]/g, char => dict[char]);
+}
