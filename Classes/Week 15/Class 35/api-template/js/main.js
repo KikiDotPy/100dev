e6af475548720682dcd7c23406a4cd0568fdf5ad
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-10-11 15:57:46 +0200

    100Dev Homework | async/await api

--- Classes/Week 15/Class 35/api-template/js/main.js
+++ Classes/Week 15/Class 35/api-template/js/main.js
@@ -1,1 +1,11 @@
 //Make an api request using async await
+
+document.getElementById('buttonApi').addEventListener('click', makeAPIRequest);
+
+
+async function makeAPIRequest() {
+    const request = await fetch('https://dog.ceo/api/breeds/image/random');
+    const response = await request.json();
+    document.querySelector('img').src = response.message
+}
+
