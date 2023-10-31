a64a204a3c1efd13e539465ea05a66b9b6043c53
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-10-31 15:47:46 +0100

    Kiki | Refactor, making code more readable

--- Classes/Week 15/Class 35/node-backend-simple-json/server.js
+++ Classes/Week 15/Class 35/node-backend-simple-json/server.js
@@ -1,78 +1,71 @@
 const http = require('http');
 const fs = require('fs')
+const path = require('path');
 const url = require('url');
 const querystring = require('querystring');
-const figlet = require('figlet')
 
 const server = http.createServer((req, res) => {
-  const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
-  console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    figlet('404!!', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+  let extName = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extName) {
+    case '.js':
+      contentType = 'application/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+  };
+
+  fs.readFile(filePath, (err, content) => {
+    if(err){
+      if(err.code === 'ENOENT') {
+        console.log(`Failed to read file: ${filePath}`); 
+        console.dir(err);
+        res.writeHead(404);
+        res.end('File not found');
+        return;
+      } else {
+        res.end(content, 'Another type of error occurred'); 
       }
-      res.write(data);
+    } 
+    
+    else if (req.url == '/api') {
+      if('student' in params){
+        if(params['student']== 'leon'){
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          const objToJson = {
+            name: "leon",
+            status: "Boss Man",
+            currentOccupation: "Baller"
+          }
+          res.end(JSON.stringify(objToJson));
+        }//student = leon
+        else if(params['student'] != 'leon'){
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          const objToJson = {
+            name: "unknown",
+            status: "unknown",
+            currentOccupation: "unknown"
+          }
+          res.end(JSON.stringify(objToJson));
+        }//student != leon
+      }//student if
+    }
+    
+    else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.write(content);
       res.end();
-    });
-  }
+    }
+  });
 });
 
-server.listen(8000);
+const PORT = process.env.PORT || 8000
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
