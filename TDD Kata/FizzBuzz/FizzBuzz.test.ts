6735e66f87236075b1d855334568182105f6a117
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-11-16 16:48:13 +0100

    Add print each number in a range

--- TDD Kata/FizzBuzz/FizzBuzz.test.ts
+++ TDD Kata/FizzBuzz/FizzBuzz.test.ts
@@ -1,14 +1,32 @@
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect, test, jest} from '@jest/globals';
+import { fizzBuzz } from './FizzBuzz';
 
 
 describe('FizzBuzz', () => {
-    test('should', () => {
-        expect(0).toBe(0);
+    test('Should print each number from 1 to 100', () => {
+        console.log = jest.fn();
+
+        fizzBuzz(1, 100);
+
+        expect(console.log).toHaveBeenCalledTimes(100);
+
+    });
+    test('Should print each number in a range of 2 numbers', () => {
+        console.log = jest.fn();
+
+        fizzBuzz(2, 6);
+
+        expect(console.log).toHaveBeenCalledTimes(5);
     });
 });
 
 
+
+
+
+
 // Write a program that prints one line for each number from 1 to 100
+
 // Usually just print the number itself.
 // For multiples of three print Fizz instead of the number
 // For the multiples of five print Buzz instead of the number
