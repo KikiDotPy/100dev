4996c32c262df670a6c245e6976e89b657abd386
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-11-21 12:53:58 +0100

    Add button on html

--- Classes/Week 15/Class37/rock-paper-scissor/js/main.js
+++ Classes/Week 15/Class37/rock-paper-scissor/js/main.js
@@ -1,13 +1,5 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+document.getElementById('startBattle').addEventListener('click', something);
 
-async function makeReq(){
-
-  const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api?student=${userName}`)
-  const data = await res.json()
-
-  console.log(data);
-  document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
+function something() {
+  console.log('CLICKED')
 }
