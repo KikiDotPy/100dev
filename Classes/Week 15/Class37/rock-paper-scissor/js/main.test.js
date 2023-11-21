9c7dcbed6c5b6869bf3d258e3e4098b42c14aa6b
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-11-21 14:43:05 +0100

    set server to handle js, css and html

--- Classes/Week 15/Class37/rock-paper-scissor/js/main.test.js
+++ Classes/Week 15/Class37/rock-paper-scissor/js/main.test.js
@@ -1,5 +1,10 @@
 describe('Main', () => {
-    it('should', ()=> {
-        expect(1).toBe(1);
+    it('Should log "CLICKED" when the Start Battle button has been pushed', ()=> {
+        const elem = document.getElementById('startBattle')
+        consoleSpy = jest.spyOn(console, 'log');
+
+        elem.click()
+
+        expect(consoleSpy).toHaveBeenNthCalledWith(1, 'CLICKED');
     })
 });
--- Classes/Week 15/Class37/rock-paper-scissor/server.js
+++ Classes/Week 15/Class37/rock-paper-scissor/server.js
@@ -1,7 +1,47 @@
 const http = require('http');
+const fs = require('fs')
+const path = require('path');
+const url = require('url');
+const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
+  const params = querystring.parse(url.parse(req.url).query);
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
 
+  let extName = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extName) {
+    case '.js':
+      contentType = 'application/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+  };
+
+  fs.readFile(filePath, (err, content) => {
+    if(err){
+      if(err.code === 'ENOENT') {
+        console.log(`Failed to read file: ${filePath}`); 
+        console.dir(err);
+        res.writeHead(404);
+        res.end('File not found');
+        return;
+      } else {
+        res.end(content, 'Another type of error occurred'); 
+      }
+    }     
+    else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.write(content);
+      res.end();
+    }
+  });
 });
 
-server.listen(8000);
+const PORT = process.env.PORT || 8000
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
