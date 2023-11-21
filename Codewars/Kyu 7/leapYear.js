abb19867ea3b917b27250295bf42df2dc67e5506
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-11-21 11:07:23 +0100

    Daily codewars solution

---
+++ Codewars/Kyu 7/leapYear.js
@@ -0,0 +1,11 @@
+function isLeapYear(year) {
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+      return true
+    } 
+    return false
+  }
+
+
+  function isLeapYear(year) {
+    return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
+  }
