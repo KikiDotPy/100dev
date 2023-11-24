48acbb4f0de79b74634bded9e94accc749b628d8
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-11-24 17:11:18 +0100

    Add connection to db with post and get

--- Classes/Week 15/Class37/CRUD api/server.js
+++ Classes/Week 15/Class37/CRUD api/server.js
@@ -2,27 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
 const connectionString = require('./sec.js');
-
-
 const app = express();
+
+MongoClient.connect(connectionString)
+.then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
 
 app.listen(3000, () => {
 
     app.use(bodyParser.urlencoded({ extended : true }));
 
     app.get('/', (req, res) => {
+        const cursor = db.collection('quotes')
+            .find()
+            .toArray()
+            .then(result => {
+                console.log(result)
+            })
         res.sendFile(__dirname + '/index.html')
         });
+        
     app.post('/quotes', (req, res) => {
-        console.log(req.body)
+        quotesCollection
+        .insertOne(req.body)
+        .then(result => {
+            res.redirect('/')
+        })
+        .catch(error => console.error(error))
+        })
     });
-
-    MongoClient.connect(connectionString)
-    .then(client => {
-        console.log('Connected to Database')
-        const db = client.db('star-wars-quotes')
-    })
-    .catch(console.error)
 
 })
 
