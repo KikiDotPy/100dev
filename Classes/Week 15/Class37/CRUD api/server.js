5f8692fe18e767510b837502dab3daafe0a42332
Chiara Di Sano <chiara.disano@thoughtworks.com>
2023-11-24 15:56:39 +0100

    Fix export bug

--- Classes/Week 15/Class37/CRUD api/server.js
+++ Classes/Week 15/Class37/CRUD api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
-const connectionString = require('/sec.js')
+const connectionString = require('./sec.js');
 
 
 const app = express();
